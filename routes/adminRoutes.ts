a082aad6a50ad2e51819de671877baf8a7de3b87
Khelsai Rajwade <119441119+khelsai01@users.noreply.github.com>
2024-07-24 22:45:06 +0530

    holidays tours backend code has been added

---
+++ routes/adminRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { createTour, deleteTour, updateTour } from '../controllers/adminController';
+
+
+const router = Router();
+
+// Create a new tour
+router.post('/tours',  createTour);
+
+// Update an existing tour
+router.patch('/tours/:id',  updateTour);
+
+// Delete a tour
+router.delete('/tours/:id', deleteTour);
+
+export default router;
+
